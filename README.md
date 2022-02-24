03d2282e972ad912aece91ed229c4900f54f19ee
Krzysztof Kadowski <kkadowski@gmail.com>
2022-02-24 15:47:33 +0100

    First walk...

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Python_QT_Postgresql
+
+QT5 app + Postgresql
---
+++ db/first_sql.sql
@@ -0,0 +1,22 @@
+--Creating a new database customers
+CREATE DATABASE customers;
+
+-- Creating admin user for database customers
+CREATE USER admin WITH ENCRYPTED PASSWORD 'adminadmin';
+GRANT ALL PRIVILEGES ON DATABASE customers TO admin;
+
+SELECT usename AS role_name,
+  CASE 
+     WHEN usesuper AND usecreatedb THEN 
+	   CAST('superuser, create database' AS pg_catalog.text)
+     WHEN usesuper THEN 
+	    CAST('superuser' AS pg_catalog.text)
+     WHEN usecreatedb THEN 
+	    CAST('create database' AS pg_catalog.text)
+     ELSE 
+	    CAST('' AS pg_catalog.text)
+  END role_attributes
+FROM pg_catalog.pg_user
+ORDER BY role_name desc;
+
+
